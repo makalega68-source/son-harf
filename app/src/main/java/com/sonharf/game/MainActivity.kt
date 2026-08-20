27334f8e3f7d3e204c728102a44f4693733ebec9
makalega68-source <makalega68@gmail.com>
2026-08-21 02:20:36 +0300

    Lock launcher to full mockup experience

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -62,7 +62,7 @@ class MainActivity : ComponentActivity() {
                 ),
                 typography = SonHarfNeonTypography,
             ) {
-                FinalNeonSonHarfApp()
+                MockupSonHarfApp()
             }
         }
     }
