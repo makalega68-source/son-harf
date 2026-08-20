5bcfacb0a36cd4fa2d6577116a1370c7b6a8cdf5
makalega68-source <makalega68@gmail.com>
2026-08-21 02:42:25 +0300

    Launch full-history neon production shell

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : ComponentActivity() {
                 typography = SonHarfNeonTypography,
             ) {
                 Box {
-                    MockupSonHarfApp()
+                    FullHistoryNeonApp()
                     WinnerFireworkOverlay()
                     FriendsQuickAccessOverlay()
                 }
