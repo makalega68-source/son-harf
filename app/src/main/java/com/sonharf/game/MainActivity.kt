12f7a6d786f07253a9e01d658b41f9b7d89e9f1d
makalega68-source <makalega68@gmail.com>
2026-08-21 02:21:23 +0300

    Use approved target neon interface

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -62,7 +62,7 @@ class MainActivity : ComponentActivity() {
                 ),
                 typography = SonHarfNeonTypography,
             ) {
-                MockupSonHarfApp()
+                FinalNeonSonHarfApp()
             }
         }
     }
