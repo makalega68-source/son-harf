c79a948aa235838da7dee0a0131f51644710b937
makalega68-source <makalega68@gmail.com>
2026-08-20 18:13:46 +0300

    Add live cosmetic runtime state

---
+++ app/src/main/java/com/sonharf/game/CosmeticRuntime.kt
@@ -0,0 +1,39 @@
+package com.sonharf.game
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.graphics.Color
+import com.sonharf.game.data.EquippedCosmeticsDto
+
+object SonHarfCosmetics {
+    var profileFrameId by mutableStateOf<String?>(null)
+    var nameStyleId by mutableStateOf<String?>(null)
+    var gameThemeId by mutableStateOf<String?>(null)
+    var keyboardThemeId by mutableStateOf<String?>(null)
+    var victoryEffectId by mutableStateOf<String?>(null)
+    var emojiPackId by mutableStateOf<String?>(null)
+
+    fun apply(e: EquippedCosmeticsDto?) {
+        profileFrameId = e?.profileFrameId
+        nameStyleId = e?.nameStyleId
+        gameThemeId = e?.gameThemeId
+        keyboardThemeId = e?.keyboardThemeId
+        victoryEffectId = e?.victoryEffectId
+        emojiPackId = e?.emojiPackId
+    }
+
+    val profileAccent: Color
+        get() = when (profileFrameId) {
+            "frame_gold" -> SonHarfGold
+            "frame_neon" -> SonHarfCyan
+            else -> SonHarfMuted
+        }
+
+    val playerNameColor: Color
+        get() = if (nameStyleId == "name_cyan") SonHarfCyan else SonHarfText
+
+    val keyboardIsNeon: Boolean get() = keyboardThemeId == "keyboard_neon"
+    val auroraTheme: Boolean get() = gameThemeId == "theme_aurora"
+    val crownVictory: Boolean get() = victoryEffectId == "victory_crown"
+}
