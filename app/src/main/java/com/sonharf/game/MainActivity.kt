45a767635ddfb1249d0a34900e8a597b7f130ad9
makalega68-source <makalega68@gmail.com>
2026-08-20 09:55:01 +0300

    Enable enhanced in-game keyboard experience

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     onBackground = SonHarfText,
                     onSurface = SonHarfText,
                 )
-            ) { AuroraSonHarfAppWithCombo() }
+            ) { AuroraSonHarfAppEnhanced() }
         }
     }
 }
