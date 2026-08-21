27e1cf55eee7367a04dd2b8a680ffefe1712fa0a
makalega68-source <makalega68@gmail.com>
2026-08-21 15:38:11 +0300

    Use approved full premium UI shell

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : ComponentActivity() {
                 typography = SonHarfNeonTypography,
             ) {
                 Box {
-                    ClassicArenaApp()
+                    ClassicPremiumApp()
                     WinnerFireworkOverlay()
                     FriendsQuickAccessOverlay()
                     GameInviteOverlay()
