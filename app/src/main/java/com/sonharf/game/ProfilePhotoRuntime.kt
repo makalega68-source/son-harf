6ce3ee1e1a1dc00a2c72940be78ac438517b6083
makalega68-source <makalega68@gmail.com>
2026-08-22 13:05:58 +0300

    Fix profile photo avatar padding import

--- app/src/main/java/com/sonharf/game/ProfilePhotoRuntime.kt
+++ app/src/main/java/com/sonharf/game/ProfilePhotoRuntime.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.sonharf.game.data.SupabaseProvider
 import io.github.jan.supabase.auth.auth
