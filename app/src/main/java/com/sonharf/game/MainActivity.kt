d94b5d2e21f0406f4ada1d7180e955c72451124e
makalega68-source <makalega68@gmail.com>
2026-08-22 16:38:52 +0300

    Route app through two-game portal

--- app/src/main/java/com/sonharf/game/MainActivity.kt
+++ app/src/main/java/com/sonharf/game/MainActivity.kt
@@ -64,7 +64,7 @@ class MainActivity : ComponentActivity() {
                 typography = SonHarfNeonTypography,
             ) {
                 Box {
-                    ClassicPremiumApp()
+                    GamePortalApp()
                     WinnerFireworkOverlay()
                     FriendsQuickAccessOverlay()
                     GameInviteOverlay()
