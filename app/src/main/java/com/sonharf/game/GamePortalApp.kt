555221c678e57356c625e79dd8712830bfc0f0e0
makalega68-source <makalega68@gmail.com>
2026-08-23 15:51:57 +0300

    Fix mascot rail Compose weight import

--- app/src/main/java/com/sonharf/game/GamePortalApp.kt
+++ app/src/main/java/com/sonharf/game/GamePortalApp.kt
@@ -1,12 +1,6 @@
 package com.sonharf.game
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.weight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
