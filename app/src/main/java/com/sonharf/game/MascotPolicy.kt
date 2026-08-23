425437e5b62cb0b69c6a79b0786e9e85cb8cc1c1
makalega68-source <makalega68@gmail.com>
2026-08-23 10:51:41 +0300

    Prevent mascot feature regression

---
+++ app/src/main/java/com/sonharf/game/MascotPolicy.kt
@@ -0,0 +1,6 @@
+package com.sonharf.game
+
+/** Permanent product policy: mascot features are disabled and must not be reintroduced. */
+internal object MascotPolicy {
+    const val ENABLED = false
+}
