1af1386f045805c5561d25cea0750f99f3ee6e21
makalega68-source <makalega68@gmail.com>
2026-08-23 12:50:21 +0300

    Add gameplay excitement regression tests

---
+++ app/src/test/java/com/sonharf/game/GameplayExcitementRulesTest.kt
@@ -0,0 +1,25 @@
+package com.sonharf.game
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Test
+
+class GameplayExcitementRulesTest {
+    @Test fun coreSonHarfRulesStayLocked() {
+        assertEquals(45, GameplayExcitementRules.TURN_SECONDS)
+        assertEquals(3, GameplayExcitementRules.CORRECT_WORD_POINTS)
+        assertEquals(-1, GameplayExcitementRules.INVALID_WORD_POINTS)
+        assertEquals(5, GameplayExcitementRules.STREAK_TARGET)
+        assertEquals(3, GameplayExcitementRules.STREAK_BONUS_POINTS)
+    }
+
+    @Test fun triviaMilestoneCountdownIsStable() {
+        assertEquals(5, GameplayExcitementRules.wordsToNextTrivia(0))
+        assertEquals(1, GameplayExcitementRules.wordsToNextTrivia(4))
+        assertEquals(5, GameplayExcitementRules.wordsToNextTrivia(5))
+    }
+
+    @Test fun mascotMustRemainDisabled() {
+        assertFalse(MascotPolicy.ENABLED)
+    }
+}
