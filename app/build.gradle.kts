0d5d1ce03c7ed2e77d77ea4838ab8141420d75fd
makalega68-source <makalega68@gmail.com>
2026-08-23 13:03:51 +0300

    Enable gameplay unit tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -97,5 +97,6 @@ dependencies {
 
     implementation("com.android.billingclient:billing-ktx:8.0.0")
     implementation("com.google.android.gms:play-services-ads:24.5.0")
+    testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
