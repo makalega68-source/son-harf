2eeab0c587790d0e49183d9012b2d98b42f429f9
makalega68-source <makalega68@gmail.com>
2026-08-26 11:20:14 +0300

    Test Eve progression math and quotas

---
+++ app/src/test/java/com/sonharf/game/EveCompanionRulesTest.kt
@@ -0,0 +1,34 @@
+package com.sonharf.game
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class EveCompanionRulesTest {
+    @Test
+    fun xpCurveUsesRequestedPowerFormula() {
+        assertEquals(100, EveCompanionRules.xpTarget(1))
+        assertEquals((100 * 2.0.pow125()).toInt(), EveCompanionRules.xpTarget(2))
+        assertEquals((100 * 10.0.pow125()).toInt(), EveCompanionRules.xpTarget(10))
+        assertTrue(EveCompanionRules.xpTarget(50) > EveCompanionRules.xpTarget(10))
+    }
+
+    @Test
+    fun interactionQuotasAndRewardsArePinned() {
+        assertEquals(5, EveCompanionRules.MAX_DAILY_FEED)
+        assertEquals(10, EveCompanionRules.MAX_DAILY_PET)
+        assertEquals(25, EveCompanionRules.FEED_XP)
+        assertEquals(10, EveCompanionRules.PET_XP)
+        assertEquals(500, EveCompanionRules.levelRewardGold(10))
+        assertEquals(11, EveCompanionRules.levelRewardDiamonds(10))
+    }
+
+    @Test
+    fun milestoneFeaturesRemainAvailable() {
+        assertEquals("Kişisel Giydirme & Kostüm Odası", EveCompanionRules.featureUnlocks[10])
+        assertEquals("Akıllı Soru İpucu Radarı (Ekstra %20 Netlik)", EveCompanionRules.featureUnlocks[20])
+        assertEquals("Efsanevi Yoldaş Rozeti & Altın Çark", EveCompanionRules.featureUnlocks[50])
+    }
+
+    private fun Double.pow125(): Double = kotlin.math.pow(this, 1.25)
+}
