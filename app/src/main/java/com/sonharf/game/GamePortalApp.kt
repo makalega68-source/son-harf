30cd2afc7cc2937f802fef25c162cb528ef41fc7
makalega68-source <makalega68@gmail.com>
2026-08-26 09:07:37 +0300

    Fix Eve travel Compose compilation

--- app/src/main/java/com/sonharf/game/GamePortalApp.kt
+++ app/src/main/java/com/sonharf/game/GamePortalApp.kt
@@ -21,7 +21,6 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
@@ -31,6 +30,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
