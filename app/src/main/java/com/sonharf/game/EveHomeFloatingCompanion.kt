dcc201ba234200b5d6caf44ab9a4637f523dcb73
makalega68-source <makalega68@gmail.com>
2026-08-26 17:32:18 +0300

    fix(eve): import pointer consume for floating drag

--- app/src/main/java/com/sonharf/game/EveHomeFloatingCompanion.kt
+++ app/src/main/java/com/sonharf/game/EveHomeFloatingCompanion.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.consume
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.IntOffset
