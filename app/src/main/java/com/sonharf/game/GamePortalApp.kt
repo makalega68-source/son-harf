e62842a086659dd682140f1575e9b22dd2e86748
makalega68-source <makalega68@gmail.com>
2026-08-27 12:33:38 +0300

    Fix scoped Column weight import for mascot banner wrapper

--- app/src/main/java/com/sonharf/game/GamePortalApp.kt
+++ app/src/main/java/com/sonharf/game/GamePortalApp.kt
@@ -1,10 +1,6 @@
 package com.sonharf.game
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.weight
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Modifier
