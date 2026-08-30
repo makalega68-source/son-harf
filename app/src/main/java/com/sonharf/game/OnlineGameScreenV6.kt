da14bb12aeddb109b67f4c286b5eee890d483f66
makalega68-source <makalega68@gmail.com>
2026-08-30 14:23:17 +0300

    Fix clickable import for rebuilt game lobby

--- app/src/main/java/com/sonharf/game/OnlineGameScreenV6.kt
+++ app/src/main/java/com/sonharf/game/OnlineGameScreenV6.kt
@@ -3,6 +3,7 @@ package com.sonharf.game
 import androidx.compose.animation.core.*
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
