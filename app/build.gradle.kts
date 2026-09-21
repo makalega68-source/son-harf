273e0e39db99d494f0756d2199269eb76e8b5497
makalega68-source <makalega68@gmail.com>
2026-09-21 03:16:04 -0700

    build: opt in to Material3 bottom sheet api for purchased store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,12 @@ android {
     }
 }
 
+kotlin {
+    compilerOptions {
+        optIn.add("androidx.compose.material3.ExperimentalMaterial3Api")
+    }
+}
+
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.08.00"))
     implementation("androidx.activity:activity-compose:1.10.1")
